27dc23ebef4cdd5ef4a3b66165cc6b62119f805a
zchapasko <ztc@bu.edu>
2016-04-29 14:41:34 -0400

    Added comments to PlaylistList

--- GUI/playlist_app/PlaylistList.cpp
+++ GUI/playlist_app/PlaylistList.cpp
@@ -1,21 +1,15 @@
 #include "PlaylistList.h"
 
-//PlaylistList::PlaylistList(){}
-
 PlaylistList::PlaylistList(string filename, song_obj *songs_ds){
     // set min popularity high so that it can be updated as nodes are added
     min_popularity = 100;
 
-    // place dummy node into list so that insert performs correctly
-    //song_obj temp();
-    //songs_ds;
+    // get pointer to song array for updating purposes
+    this -> songs_ds = songs_ds; 
 
-    //PlaylistList list_test("day00.txt");
-    this -> songs_ds = songs_ds;
-
+    //place dummy node into list so that insert performs correctly
     playlist_obj temp;
     temp.popularity = 0;
-    //temp.songs.push_back(*(songs_ds + 0));
     ll.insert(ll.begin(),temp);
 
     // add real playlists here
@@ -23,6 +17,7 @@ PlaylistList::PlaylistList(string filename, song_obj *songs_ds){
 }
 
 bool PlaylistList::insert_playlist(playlist_obj node_to_add){
+    // if the playlist doesn't meet minimum popularity cutoff just exit
     if(ll.size() >= MAX_PLAYLISTS && node_to_add.popularity < min_popularity){
         return false;
     }
@@ -38,11 +33,11 @@ bool PlaylistList::insert_playlist(playlist_obj node_to_add){
             // get pointer to point to playlist that was just added
             --it;
 
-            // update here
+            // update song array here
             vector<song_obj*> temp_songs = it -> songs;
             vector<song_obj*>::iterator jj;
             for(jj = temp_songs.begin(); jj != temp_songs.end(); ++jj){
-                (*jj) -> insertTopPlaylist(&(*it)); // new line
+                (*jj) -> insertTopPlaylist(&(*it));
                 songs_ds[(*jj) -> index].popularity += it -> popularity;
             }
 
@@ -53,11 +48,13 @@ bool PlaylistList::insert_playlist(playlist_obj node_to_add){
         }
     }
 
+    // should never reach here
     qDebug() << "insert into linked list fell through all cases, error" << endl;
     return false;
 }
 
 bool PlaylistList::delete_playlists(){
+    // if list contains fewer than max playlists, don't delete
     if(ll.size() <= MAX_PLAYLISTS){
         return false;
     }
@@ -66,6 +63,7 @@ bool PlaylistList::delete_playlists(){
     vector<song_obj*> temp;
     vector<song_obj*>::iterator jj;
 
+    // delete playlists one by one from end of list
     int nodes_to_delete = ll.size() - MAX_PLAYLISTS;
     while(nodes_to_delete > 0){
         --nodes_to_delete;
@@ -78,9 +76,11 @@ bool PlaylistList::delete_playlists(){
             songs_ds[(*jj) -> index].popularity -= ii -> popularity;
         }
 
+        // erase element
         ll.erase(ii);
     }
 
+    // update minimum popularity
     min_popularity = (--ll.end()) -> popularity;
     return true;
 }
@@ -101,13 +101,13 @@ bool PlaylistList::insert_playlists(string filename){
             first = 0;
             second = line.find(" ");
             while(second!=string::npos){
-                index = atoi(line.substr(first, second - first).c_str()); // add song
-                temp.songs.push_back(&songs_ds[index]);
+                index = atoi(line.substr(first, second - first).c_str()); // get song index
+                temp.songs.push_back(&songs_ds[index]); // add song
                 first = second + 1;
                 second = line.find(" ", first);
             }
             first++;
-            temp.popularity = atoi(line.substr(first).c_str()); // set popularity
+            temp.popularity = atoi(line.substr(first).c_str()); // add popularity
 
             // test printing here
             // for(it = temp.songs.begin(); it != temp.songs.end(); ++it){
@@ -116,10 +116,10 @@ bool PlaylistList::insert_playlists(string filename){
             // qDebug() << "   " << temp.popularity << endl;
 
             insert_playlist(temp);  // insert playlist
-            temp.songs.clear();  // clear song list to prepare for overwrite
+            temp.songs.clear();  // clear song list to prepare for temp variable overwrite
         }
         playlist_file.close();
-        delete_playlists();
+        delete_playlists(); // call delete to clean up list if list has more than maximum allowed playlists
         return true;
     } else {
         cout << "Error reading file!" << endl;
@@ -140,6 +140,7 @@ vector<playlist_obj*> PlaylistList::get_top_eight(bool print){
     list<playlist_obj>::iterator it = ll.begin();
     vector<playlist_obj*> top_eight;
 
+    // populate vector of 8 playlist objects from list
     for(ii = 0; ii < 8; ii++, it++){
         top_eight.push_back(&(*it));
     }
